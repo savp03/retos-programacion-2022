2aea4699a2054ddae8181339197e12866883efbb
Sergio Vargas <sergioperalta04@hotmail.com>
2022-12-29 17:05:01 -0500

    Agregamos nombres de los retos

--- soluciones/01reto.py
+++ soluciones/01reto.py
@@ -1,11 +1,11 @@
-# /*
+#Reto #1: ¿ES UN ANAGRAMA?
+
 #  * Escribe una función que reciba dos palabras (String) y retorne
 #  * verdadero o falso (Bool) según sean o no anagramas.
 #  * - Un Anagrama consiste en formar una palabra reordenando TODAS
 #  *   las letras de otra palabra inicial.
 #  * - NO hace falta comprobar que ambas palabras existan.
 #  * - Dos palabras exactamente iguales no son anagrama.
-#  */
 
 def run():
 
